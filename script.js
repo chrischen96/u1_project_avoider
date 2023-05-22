fd7d04abea4ef085ef0916b66650e69fcfbe4687
chrischen <chrischen96@outlook.com>
2023-05-22 10:47:51 -0700

    const and states

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
 console.log('working')
 const canvas = document.querySelector('.canvas')
 console.log(typeof canvas)
-for (let i = 0; i < 150; i++) {
-    let block = document.createElement('div')
-    block.innerText = i
-    canvas.appendChild(block)
+
+// define constant and variable
+const cells = Array.from(document.querySelectorAll('.cell'))
+const scoreDisplay = document.querySelector('.score')
+const enemyCells = cells.slice(0, 33)
+const playerCells = cells.slice(33)
+console.log(cells)
+
+let dropCount, speed, score
+
+// add event listener
+document.addEventListener('keydown', e => {
+    if (!dropCount) {
+        startGame()
+    }
+    const player = document.querySelector('.player')
+    if (e.key == 'ArrowRight') {
+        player.parentElement.nextElementSibling.appendChild(player)
+    } 
+    if (e.key == 'ArrowLeft') {
+        player.parentElement.previousElementSibling.appendChild(player)
+    }
+})
+
+init()
+function init() {
+    dropCount = 0
+    speed = 1000
+    score = 0
+    scoreDisplay.innerHTML = '0'
+
+    cells.forEach(cell => cell.innerHTML = '')
+    playerCells[1].innerHTML = '<div class="player"></div>'
+}
+
+function startGame() {
+    init()
+    loop()
+}
+
+function loop() {
+    let stopGame = false;
+
+    // 
+
+    if (stopGame) {
+        alert(`Your score: ${score}. Close this window to play again.`) } else {
+            dropCount ++
+            setTimeout(loop, speed)
+        }
 }
