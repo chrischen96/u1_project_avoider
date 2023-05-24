3aa39bba08c411e6073f10a5f851bc7d3d235a1d
chrischen <chrischen96@outlook.com>
2023-05-24 08:07:38 -0700

    fixed the issue caused by the opacity property where users might click an invisible button

--- script.js
+++ script.js
@@ -19,31 +19,33 @@ console.log(cells)
 
 let dropCount, speed, score, gameOver, pauseGame, level
 
+init()
+
 // add event listener
+document.querySelector('.init').addEventListener('click', init)
 document.addEventListener('keydown', e => {
-    console.log(e.key, !dropCount, pauseGame)
+    console.log(e.key, !dropCount, dropCount, speed, score, gameOver, pauseGame, level)
     if (gameOver) return
-    const player = document.querySelector('.player')
+    const player = document.querySelector('.player') 
     if (!dropCount || pauseGame) {
-        messageStart.style.opacity = 0
+        messageStart.style.display = 'none'
         canvas.style.opacity = 0.9
         pauseGame = false
         console.log('drop')
-        setTimeout(drop, speed)
+        drop()
+        console.log('after drop') 
     } else if (e.key == ' ') {
-        console.log('paused')
+        console.log('pause?'+pauseGame)
         pauseGame = true
+        console.log('pause?'+pauseGame)
     } else if (e.key == 'ArrowRight' && player.parentElement != playerCells[playerCells.length - 1]) {
         player.parentElement.nextElementSibling.appendChild(player)
     } else if (e.key == 'ArrowLeft' && player.parentElement != playerCells[0]) {
         player.parentElement.previousElementSibling.appendChild(player)
     }
 })
-document.querySelector('.init').addEventListener('click', init)
 
-init()
 function init() {
-    
     dropCount = 0
     speed = 600
     score = 0
@@ -52,21 +54,21 @@ function init() {
     pauseGame = false
     scoreDisplay.innerHTML = score
     levelDisplay.innerHTML = level
-    message.style.opacity = 0
-    messageStart.innerHTML = 'Press any key to start!'
-    messageStart.style.opacity = 0.9
+    message.style.display = 'none'
+    messageStart.innerHTML = `Press any key to start! <br />Use space key to pause the game`
+    messageStart.style.display = 'block'
     canvas.style.opacity = 0.6
     cells.forEach(cell => cell.innerHTML = '')
     playerCells[2].innerHTML = `<div class="player"><svg xmlns="http://www.w3.org/2000/svg" width="calc(60vh / 12)" height="calc(60vh / 12)" fill="currentColor" class="bi bi-airplane-engines-fill" viewBox="0 0 16 16">
     <path d="M8 0c-.787 0-1.292.592-1.572 1.151A4.347 4.347 0 0 0 6 3v3.691l-2 1V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.191l-1.17.585A1.5 1.5 0 0 0 0 10.618V12a.5.5 0 0 0 .582.493l1.631-.272.313.937a.5.5 0 0 0 .948 0l.405-1.214 2.21-.369.375 2.253-1.318 1.318A.5.5 0 0 0 5.5 16h5a.5.5 0 0 0 .354-.854l-1.318-1.318.375-2.253 2.21.369.405 1.214a.5.5 0 0 0 .948 0l.313-.937 1.63.272A.5.5 0 0 0 16 12v-1.382a1.5 1.5 0 0 0-.83-1.342L14 8.691V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v.191l-2-1V3c0-.568-.14-1.271-.428-1.849C9.292.591 8.787 0 8 0Z"/>
 </svg></div>`
-    console.log('initialized')
+    console.log('initialized', !dropCount, pauseGame)
 }
 
 function drop() {
     if (pauseGame) {
         messageStart.innerHTML = 'Press any key to resume!'
-        messageStart.style.opacity = 0.9
+        messageStart.style.display = 'block'
         canvas.style.opacity = 0.6
         return
     }
@@ -104,7 +106,7 @@ function drop() {
     if (gameOver) {
         console.log('game over')
         document.querySelector('.message p span').innerHTML = score
-        message.style.opacity = 0.9
+        message.style.display = 'grid'
         canvas.style.opacity = 0.6
     } else {
         dropCount ++
