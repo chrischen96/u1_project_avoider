b639eb0af6311fda243828aa814232958db89074
chrischen <chrischen96@outlook.com>
2023-05-24 04:02:38 -0700

    pause/resume, change font

--- script.js
+++ script.js
@@ -17,67 +17,69 @@ const message = document.querySelector('.message')
 const messageStart = document.querySelector('.messageStart')
 console.log(cells)
 
-let dropCount, speed, score, stopGame, pauseGame, level
+let dropCount, speed, score, gameOver, pauseGame, level
 
 // add event listener
 document.addEventListener('keydown', e => {
-    if (stopGame) return
+    console.log(e.key, !dropCount, pauseGame)
+    if (gameOver) return
+    const player = document.querySelector('.player')
     if (!dropCount || pauseGame) {
         messageStart.style.opacity = 0
         canvas.style.opacity = 0.9
         pauseGame = false
-        drop()
-    }
-    const player = document.querySelector('.player')
-    if (e.key == 'ArrowRight' && player.parentElement != playerCells[playerCells.length - 1]) {
-        player.parentElement.nextElementSibling.appendChild(player)
-    } 
-    if (e.key == 'ArrowLeft' && player.parentElement != playerCells[0]) {
-        player.parentElement.previousElementSibling.appendChild(player)
-    }
-
-    if (e.key == ' ') { 
+        console.log('drop')
+        setTimeout(drop, speed)
+    } else if (e.key == ' ') {
+        console.log('paused')
         pauseGame = true
+    } else if (e.key == 'ArrowRight' && player.parentElement != playerCells[playerCells.length - 1]) {
+        player.parentElement.nextElementSibling.appendChild(player)
+    } else if (e.key == 'ArrowLeft' && player.parentElement != playerCells[0]) {
+        player.parentElement.previousElementSibling.appendChild(player)
     }
 })
 document.querySelector('.init').addEventListener('click', init)
 
 init()
 function init() {
+    
     dropCount = 0
-    speed = 800
+    speed = 600
     score = 0
     level = 1
-    stopGame = false
+    gameOver = false
     pauseGame = false
     scoreDisplay.innerHTML = score
     levelDisplay.innerHTML = level
     message.style.opacity = 0
-    messageStart.style.opacity = 0.8
-    canvas.style.opacity = 0.5
+    messageStart.innerHTML = 'Press any key to start!'
+    messageStart.style.opacity = 0.9
+    canvas.style.opacity = 0.6
     cells.forEach(cell => cell.innerHTML = '')
     playerCells[2].innerHTML = `<div class="player"><svg xmlns="http://www.w3.org/2000/svg" width="calc(60vh / 12)" height="calc(60vh / 12)" fill="currentColor" class="bi bi-airplane-engines-fill" viewBox="0 0 16 16">
     <path d="M8 0c-.787 0-1.292.592-1.572 1.151A4.347 4.347 0 0 0 6 3v3.691l-2 1V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.191l-1.17.585A1.5 1.5 0 0 0 0 10.618V12a.5.5 0 0 0 .582.493l1.631-.272.313.937a.5.5 0 0 0 .948 0l.405-1.214 2.21-.369.375 2.253-1.318 1.318A.5.5 0 0 0 5.5 16h5a.5.5 0 0 0 .354-.854l-1.318-1.318.375-2.253 2.21.369.405 1.214a.5.5 0 0 0 .948 0l.313-.937 1.63.272A.5.5 0 0 0 16 12v-1.382a1.5 1.5 0 0 0-.83-1.342L14 8.691V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v.191l-2-1V3c0-.568-.14-1.271-.428-1.849C9.292.591 8.787 0 8 0Z"/>
-  </svg></div>`
+</svg></div>`
+    console.log('initialized')
 }
 
 function drop() {
     if (pauseGame) {
         messageStart.innerHTML = 'Press any key to resume!'
-        messageStart.style.opacity = 0.8
-        canvas.style.opacity = 0.5
+        messageStart.style.opacity = 0.9
+        canvas.style.opacity = 0.6
         return
     }
     for (let i = enemyCells.length - 1; i >= 0; i--) {
         const cell = enemyCells[i]
-        const nextCell = cells[i + 5]
         const enemy = cell.children[0]
+        const nextCell = cells[i + 5]
         if (!enemy) continue
         nextCell.appendChild(enemy)
         // collide
         if (playerCells.includes(nextCell)) {
             if (nextCell.querySelector('.player')) {
-                stopGame = true
+                gameOver = true
                 document.querySelector('.player').innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="calc(50vh / 12)" height="calc(50vh / 12)" fill="currentColor" class="bi bi-heartbreak-fill" viewBox="0 0 16 16">
                 <path d="M8.931.586 7 3l1.5 4-2 3L8 15C22.534 5.396 13.757-2.21 8.931.586ZM7.358.77 5.5 3 7 7l-1.5 3 1.815 4.537C-6.533 4.96 2.685-2.467 7.358.77Z"/>
               </svg>`
@@ -86,7 +88,7 @@ function drop() {
                 scoreDisplay.innerHTML = score
                 level = 1 + Math.floor(score / 5)
                 levelDisplay.innerHTML = level
-                speed = Math.max(100, 800 - 100 * (level - 1))
+                speed = Math.max(100, 600 - 100 * (level - 1))
             }
             enemy.remove()
         }
@@ -99,9 +101,11 @@ function drop() {
       </svg></div>`
     }
 
-    if (stopGame) {
+    if (gameOver) {
+        console.log('game over')
         document.querySelector('.message p span').innerHTML = score
-        message.style.opacity = 0.8
+        message.style.opacity = 0.9
+        canvas.style.opacity = 0.6
     } else {
         dropCount ++
         console.log(dropCount)
